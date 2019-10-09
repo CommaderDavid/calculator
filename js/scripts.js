ed27c41dadcd1f22402a8b4d39a1250d9e2658ee
David Boedigheimer <stealth259@gmail.com>
2019-10-09 11:13:08 -0700

    Add console logs to test if data is coming through.

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,9 @@ $(document).ready(function() {
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
     var operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1);
+    console.log("2nd number: " + number2);
+    console.log("operator: " + operator);
     var result = add(number1, number2);
     $("#output").text(result);
   });
