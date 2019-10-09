5cda98e4481f4fe1196c128b65bdb14645b6df60
David Boedigheimer <stealth259@gmail.com>
2019-10-09 11:11:35 -0700

    Add new format to how the calculator works.

--- js/scripts.js
+++ js/scripts.js
@@ -19,35 +19,12 @@ var multiply = function(number1, number2) {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = (add(number1, number2));
-    $("#outputAdd").text(result);
-  });
-
-  $("form#sub").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = (subtract(number1, number2));
-    $("#outputSub").text(result);
-  });
-
-  $("form#div").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = (divide(number1, number2));
-    $("#outputDiv").text(result);
-  });
-
-  $("form#mult").submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($("#mult1").val());
-    var number2 = parseInt($("#mult2").val());
-    var result = (multiply(number1, number2));
-    $("#outputMult").text(result);
+  $("form#calculator").submit(function(e) {
+    e.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
 });
